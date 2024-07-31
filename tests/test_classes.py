842f46939f0c9ac15742deae337b7cce93ab0f4e
Mike Benson <116185051+ciioprof0@users.noreply.github.com>
2024-07-31 09:35:39 -0700

    Update test_classes.py to pytest

--- tests/test_classes.py
+++ tests/test_classes.py
@@ -1,61 +1,57 @@
-import unittest
+# tests/test_classes.py
 
-from model.corpus import Corpus
+import pytest
 from model.enums import *
 from model.lexical_entry import Lexeme
 from model.noun import Noun
 from model.verb import FiniteVerb
 from model.word import Word
+from model.corpus import Corpus
 
-
-# from app.stixd_classes import (
-#     PartOfSpeech, Case, Gender, Number, Person, Tense, Mood, Voice, Transitivity, Language,
-#     Lexeme, Word, FiniteVerb, Noun, Corpus
-# )
-
-class TestEnumerations(unittest.TestCase):
+class TestEnumerations:
     def test_part_of_speech_enum(self):
-        self.assertEqual(PartOfSpeech.ADJ.value, "ADJ")
-        self.assertEqual(PartOfSpeech.NOUN.name, "NOUN")
+        assert PartOfSpeech.ADJ.value == "ADJ"
+        assert PartOfSpeech.NOUN.name == "NOUN"
 
     def test_case_enum(self):
-        self.assertEqual(Case.ACCUSATIVE.value, "accusative")
-        self.assertEqual(Case.DATIVE.name, "DATIVE")
+        assert Case.ACCUSATIVE.value == "accusative"
+        assert Case.DATIVE.name == "DATIVE"
 
     def test_gender_enum(self):
-        self.assertEqual(Gender.FEMININE.value, "feminine")
-        self.assertEqual(Gender.NEUTER.name, "NEUTER")
+        assert Gender.FEMININE.value == "feminine"
+        assert Gender.NEUTER.name == "NEUTER"
 
     def test_number_enum(self):
-        self.assertEqual(Number.SINGULAR.value, "singular")
-        self.assertEqual(Number.PLURAL.name, "PLURAL")
+        assert Number.SINGULAR.value == "singular"
+        assert Number.PLURAL.name == "PLURAL"
 
     def test_person_enum(self):
-        self.assertEqual(Person.FIRST_PERSON.value, "first_person")
-        self.assertEqual(Person.THIRD_PERSON.name, "THIRD_PERSON")
+        assert Person.FIRST_PERSON.value == "first_person"
+        assert Person.THIRD_PERSON.name == "THIRD_PERSON"
 
     def test_tense_enum(self):
-        self.assertEqual(Tense.PRESENT.value, "present")
-        self.assertEqual(Tense.FUTURE.name, "FUTURE")
+        assert Tense.PRESENT.value == "present"
+        assert Tense.FUTURE.name == "FUTURE"
 
     def test_mood_enum(self):
-        self.assertEqual(Mood.INDICATIVE.value, "indicative")
-        self.assertEqual(Mood.SUBJUNCTIVE.name, "SUBJUNCTIVE")
+        assert Mood.INDICATIVE.value == "indicative"
+        assert Mood.SUBJUNCTIVE.name == "SUBJUNCTIVE"
 
     def test_voice_enum(self):
-        self.assertEqual(Voice.ACTIVE.value, "active")
-        self.assertEqual(Voice.REFLEXIVE.name, "REFLEXIVE")
+        assert Voice.ACTIVE.value == "active"
+        assert Voice.REFLEXIVE.name == "REFLEXIVE"
 
     def test_transitivity_enum(self):
-        self.assertEqual(Transitivity.INTRANSITIVE.value, "intransitive")
-        self.assertEqual(Transitivity.DITRANSITIVE3.name, "DITRANSITIVE3")
+        assert Transitivity.INTRANSITIVE.value == "intransitive"
+        assert Transitivity.DITRANSITIVE3.name == "DITRANSITIVE3"
 
     def test_language_enum(self):
-        self.assertEqual(Language.EN.value, "en")
-        self.assertEqual(Language.EN.name, "EN")
+        assert Language.EN.value == "en"
+        assert Language.EN.name == "EN"
 
-class TestLexeme(unittest.TestCase):
-    def setUp(self):
+class TestLexeme:
+    @pytest.fixture(autouse=True)
+    def setup(self):
         self.base_form = "run"
         self.pos = PartOfSpeech.VERB
         self.definition = "to move swiftly on foot"
@@ -64,14 +60,15 @@ class TestLexeme(unittest.TestCase):
         self.lex_entry = Lexeme(self.base_form, self.pos, self.definition, self.synonyms, self.antonyms)
 
     def test_lexical_entry_attributes(self):
-        self.assertEqual(self.lex_entry.base_form, self.base_form)
-        self.assertEqual(self.lex_entry.pos, self.pos)
-        self.assertEqual(self.lex_entry.definition, self.definition)
-        self.assertEqual(self.lex_entry.synonyms, self.synonyms)
-        self.assertEqual(self.lex_entry.antonyms, self.antonyms)
+        assert self.lex_entry.base_form == self.base_form
+        assert self.lex_entry.pos == self.pos
+        assert self.lex_entry.definition == self.definition
+        assert self.lex_entry.synonyms == self.synonyms
+        assert self.lex_entry.antonyms == self.antonyms
 
-class TestWord(unittest.TestCase):
-    def setUp(self):
+class TestWord:
+    @pytest.fixture(autouse=True)
+    def setup(self):
         self.base_form = "run"
         self.pos = PartOfSpeech.VERB
         self.definition = "to move swiftly on foot"
@@ -80,11 +77,12 @@ class TestWord(unittest.TestCase):
         self.word = Word(self.lex_entry, self.surface_form)
 
     def test_word_attributes(self):
-        self.assertEqual(self.word.lex_entry, self.lex_entry)
-        self.assertEqual(self.word.surface_form, self.surface_form)
+        assert self.word.lex_entry == self.lex_entry
+        assert self.word.surface_form == self.surface_form
 
-class TestFiniteVerb(unittest.TestCase):
-    def setUp(self):
+class TestFiniteVerb:
+    @pytest.fixture(autouse=True)
+    def setup(self):
         self.base_form = "run"
         self.pos = PartOfSpeech.VERB
         self.definition = "to move swiftly on foot"
@@ -98,16 +96,17 @@ class TestFiniteVerb(unittest.TestCase):
         self.finite_verb = FiniteVerb(self.lex_entry, self.surface_form, self.person, self.number, self.tense, self.voice, self.mood)
 
     def test_finite_verb_attributes(self):
-        self.assertEqual(self.finite_verb.lex_entry, self.lex_entry)
-        self.assertEqual(self.finite_verb.surface_form, self.surface_form)
-        self.assertEqual(self.finite_verb.person, self.person)
-        self.assertEqual(self.finite_verb.number, self.number)
-        self.assertEqual(self.finite_verb.tense, self.tense)
-        self.assertEqual(self.finite_verb.voice, self.voice)
-        self.assertEqual(self.finite_verb.mood, self.mood)
+        assert self.finite_verb.lex_entry == self.lex_entry
+        assert self.finite_verb.surface_form == self.surface_form
+        assert self.finite_verb.person == self.person
+        assert self.finite_verb.number == self.number
+        assert self.finite_verb.tense == self.tense
+        assert self.finite_verb.voice == self.voice
+        assert self.finite_verb.mood == self.mood
 
-class TestNoun(unittest.TestCase):
-    def setUp(self):
+class TestNoun:
+    @pytest.fixture(autouse=True)
+    def setup(self):
         self.base_form = "dog"
         self.pos = PartOfSpeech.NOUN
         self.definition = "a domesticated carnivorous mammal"
@@ -119,14 +118,15 @@ class TestNoun(unittest.TestCase):
         self.noun = Noun(self.lex_entry, self.surface_form, self.case, self.number, self.gender)
 
     def test_noun_attributes(self):
-        self.assertEqual(self.noun.lex_entry, self.lex_entry)
-        self.assertEqual(self.noun.surface_form, self.surface_form)
-        self.assertEqual(self.noun.case, self.case)
-        self.assertEqual(self.noun.number, self.number)
-        self.assertEqual(self.noun.gender, self.gender)
+        assert self.noun.lex_entry == self.lex_entry
+        assert self.noun.surface_form == self.surface_form
+        assert self.noun.case == self.case
+        assert self.noun.number == self.number
+        assert self.noun.gender == self.gender
 
-class TestCorpus(unittest.TestCase):
-    def setUp(self):
+class TestCorpus:
+    @pytest.fixture(autouse=True)
+    def setup(self):
         self.url = "http://example.com"
         self.page_hash = "abc123"
         self.text = "This is a test text."
@@ -158,20 +158,11 @@ class TestCorpus(unittest.TestCase):
         )
 
     def test_corpus_attributes(self):
-        self.assertEqual(self.corpus_entry.url, self.url)
-        self.assertEqual(self.corpus_entry.page_hash, self.page_hash)
-        self.assertEqual(self.corpus_entry.text, self.text)
-        self.assertEqual(self.corpus_entry.metadata, self.metadata)
-        self.assertEqual(self.corpus_entry.date_scraped, self.date_scraped)
-        self.assertEqual(self.corpus_entry.tokens, self.tokens)
-        self.assertEqual(self.corpus_entry.pos_tags, self.pos_tags)
-        self.assertEqual(self.corpus_entry.named_entities, self.named_entities)
-        self.assertEqual(self.corpus_entry.processed_text, self.processed_text)
-        self.assertEqual(self.corpus_entry.lemmas, self.lemmas)
-        self.assertEqual(self.corpus_entry.vocabulary, self.vocabulary)
-        self.assertEqual(self.corpus_entry.sentiment, self.sentiment)
-        self.assertEqual(self.corpus_entry.keywords, self.keywords)
-
-if __name__ == "__main__":
-    unittest.main()
-
+        assert self.corpus_entry.url == self.url
+        assert self.corpus_entry.page_hash == self.page_hash
+        assert self.corpus_entry.text == self.text
+        assert self.corpus_entry.metadata == self.metadata
+        assert self.corpus_entry.date_scraped == self.date_scraped
+        assert self.corpus_entry.tokens == self.tokens
+        assert self.corpus_entry.pos_tags == self.pos_tags
+  
